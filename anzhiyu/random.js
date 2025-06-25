04b1cdd260178286ba06bc1f801223a31772b696
Oppen-D <2066067998@qq.com>
2025-06-25 17:41:40 +0800

    Site updated: 2025-06-25 17:41:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/573d9824.html","post/56a1db5a.html","post/dae9c85.html","post/f29265a7.html"];function toRandomPost(){
+var posts=["post/573d9824.html","post/dae9c85.html","post/f29265a7.html","post/56a1db5a.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
