820117f8c28f1d4a85d72f82ea233663699764e1
Oppen-D <2066067998@qq.com>
2025-06-25 17:40:18 +0800

    Site updated: 2025-06-25 17:40:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/01/hello-world/"];function toRandomPost(){
+var posts=["post/573d9824.html","post/56a1db5a.html","post/dae9c85.html","post/f29265a7.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
