63051da55a4de70d8b931bc98aa93589023bc727
Oppen-D <2066067998@qq.com>
2025-07-15 21:48:50 +0800

    Site updated: 2025-07-15 21:48:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/8614768.html","post/56a1db5a.html","post/573d9824.html","post/3038d3bd.html","post/f29265a7.html","post/undefined.html","post/dae9c85.html"];function toRandomPost(){
+var posts=["post/56a1db5a.html","post/573d9824.html","post/8614768.html","post/3038d3bd.html","post/undefined.html","post/f29265a7.html","post/dae9c85.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
