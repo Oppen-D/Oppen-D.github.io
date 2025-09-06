cd3e4adca65e84a8586e669359f7a8905605f115
Oppen-D <2066067998@qq.com>
2025-09-06 23:23:17 +0800

    Site updated: 2025-09-06 23:23:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/573d9824.html","post/c1b18207.html","post/f3c80c6e.html","post/5b8bfab9.html","post/d8cbea87.html","post/8614768.html","post/dae9c85.html","post/6e5c3ba8.html","post/3038d3bd.html","post/ae4c1a33.html","post/56a1db5a.html","post/849df229.html"];function toRandomPost(){
+var posts=["post/f3c80c6e.html","post/573d9824.html","post/27ab86eb.html","post/c1b18207.html","post/d8cbea87.html","post/5b8bfab9.html","post/8614768.html","post/6e5c3ba8.html","post/3038d3bd.html","post/8ccb325e.html","post/56a1db5a.html","post/849df229.html","post/ae4c1a33.html","post/dae9c85.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
